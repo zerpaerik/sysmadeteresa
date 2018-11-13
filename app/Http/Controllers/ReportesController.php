66274bba987273c70c2d4c03e29c553ff0a6b32b
Emmanuel Franquiz <emmanueljfranquizs@gmail.com>
2018-11-13 15:22:44 -0400

    edicion

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -128,8 +128,8 @@ class ReportesController extends Controller
    
         
         $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
-        $objWriter->save(storage_path('helloWorld.docx'));
-        return response()->download(storage_path('helloWorld.docx'));
+        $objWriter->save(storage_path('informe.docx'));
+        return response()->download(storage_path('informe.docx'));
 
     }
 
