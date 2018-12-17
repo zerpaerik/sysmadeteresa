55ff8a8a29409547c51b424e877eeaaab79e1307
Emmanuel Franquiz <emmanueljfranquizs@gmail.com>
2018-12-17 17:19:04 -0400

    tamaño reducio de ticket

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -164,6 +164,7 @@ class ReportesController extends Controller
         $ticket =ReportesController::verTicket($id);
         $view = \View::make('reportes.ticket_atencion_ver')->with('ticket', $ticket);
         $pdf = \App::make('dompdf.wrapper');
+        $pdf->setPaper(array(0,0,867.00,343.80));
         $pdf->loadHTML($view);
         return $pdf->stream('ticket_ver');
     }
