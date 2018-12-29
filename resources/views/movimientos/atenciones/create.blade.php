6729cedb92943d30b339767c52b516b6084ec8c0
Erik Zerpa <zerpaerik@gmail.com>
2018-12-29 14:14:06 -0400

    paciente aten

--- resources/views/movimientos/atenciones/create.blade.php
+++ resources/views/movimientos/atenciones/create.blade.php
@@ -33,7 +33,7 @@
 							<select id="el1" name="id_paciente">
 								@foreach($pacientes as $pac)
 									<option value="{{$pac->id}}">
-										{{$pac->nombres}} {{$pac->apellidos}}-{{$pac->dni}}
+										{{$pac->apellidos}} {{$pac->nombres}}-{{$pac->dni}}
 									</option>
 								@endforeach
 							</select>
