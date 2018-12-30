26aa3d97bb015cfdd9ca1fc5a4d0aed8bb515b6e
Erik Zerpa <zerpaerik@gmail.com>
2018-12-29 21:45:51 -0400

    ssss

--- app/Http/Controllers/ReporteIngresosController.php
+++ app/Http/Controllers/ReporteIngresosController.php
@@ -66,7 +66,7 @@ class ReporteIngresosController extends Controller
         ->where('b.nombres','like','%'.$nom.'%')
         ->where('b.apellidos','like','%'.$ape.'%')
         ->whereNotIn('a.monto',[0,0.00])
-        ->whereNotIn('a.porcentaje',[0,0.00])
+        //->whereNotIn('a.porcentaje',[0,0.00])
 		->whereBetween('a.created_at', [date('Y-m-d 00:00:00', strtotime($request->inicio)), date('Y-m-d 23:59:59', strtotime($request->final))])
         //->whereBetween('a.created_at', [date('Y-m-d 00:00:00', strtotime($initial)), date('Y-m-d 23:59:59', strtotime($initial))])
         //->whereBetween('a.created_at', [date('Y-m-d 00:00:00', strtotime($final)), date('Y-m-d 23:59:59', strtotime($final))])
