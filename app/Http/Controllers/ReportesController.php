bf3ca6a2e450e3d40c17dd75b6ee8303dd4b8de9
Erik Zerpa <zerpaerik@gmail.com>
2019-01-04 11:19:49 -0400

    tic

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -165,6 +165,7 @@ class ReportesController extends Controller
         $ticket =ReportesController::verTicket($id);
         $view = \View::make('reportes.ticket_atencion_ver')->with('ticket', $ticket);
         $pdf = \App::make('dompdf.wrapper');
+        $pdf->setPaper('A5', 'landscape');
 		//$pdf->setPaper(array(0,0,600.00,360.00));
         $pdf->loadHTML($view);
         return $pdf->stream('ticket_ver');
