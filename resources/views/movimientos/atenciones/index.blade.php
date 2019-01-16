9fbc75496c61fb4c4857566df7ebacb11ece4e50
Erik Zerpa <zerpaerik@gmail.com>
2019-01-16 17:23:46 -0400

    ccccc

--- resources/views/movimientos/atenciones/index.blade.php
+++ resources/views/movimientos/atenciones/index.blade.php
@@ -64,7 +64,7 @@
 						@endif
 						<td>{{$d->monto}}</td>
 						<td>{{$d->abono}}</td>s
-						<td>{{date('d-m-Y', strtotime($d->created_at))}}</td>
+						<td>{{date('d-m-Y H:i', strtotime($d->created_at))}}</td>
 						<td>{{$d->user}},{{$d->userp}}</td>
 		                <td><a target="_blank" class="btn btn-primary" href="{{$model1.'-ver-'.$d->id}}">Ver Ticket</a></td>	
 						      @if(\Auth::user()->role_id <> 6)							 
