6ce05a5b4363888e7c3f6846c89942899e5a3bfc
Erik Zerpa <zerpaerik@gmail.com>
2019-01-20 20:27:24 -0400

    ccccc

--- resources/views/movimientos/atenciones/index.blade.php
+++ resources/views/movimientos/atenciones/index.blade.php
@@ -44,6 +44,7 @@
 							<th>Monto</th>
 							<th>Monto Abonado</th>
 							<th>Fecha</th>
+							<th>TP</th>
 							<th>Registrado Por:</th>
 							<th>Acciones</th>
 							<th></th>
@@ -63,8 +64,9 @@
 						<td>{{$d->paquete}}</td>
 						@endif
 						<td>{{$d->monto}}</td>
-						<td>{{$d->abono}}</td>s
+						<td>{{$d->abono}}</td>
 						<td>{{date('d-m-Y H:i', strtotime($d->created_at))}}</td>
+						<td>{{$d->tipo_ingreso}}</td>
 						<td>{{$d->user}},{{$d->userp}}</td>
 		                <td><a target="_blank" class="btn btn-primary" href="{{$model1.'-ver-'.$d->id}}">Ver Ticket</a></td>	
 						      @if(\Auth::user()->role_id <> 6)							 
