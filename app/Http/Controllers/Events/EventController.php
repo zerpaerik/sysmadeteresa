28ab4737be7ce68a67ccb9fbf4503e4c06730d4b
Diego <diegoarmando957@hotmail.com>
2019-04-15 14:07:42 -0400

    prenatal create/ficha/show fix

--- app/Http/Controllers/Events/EventController.php
+++ app/Http/Controllers/Events/EventController.php
@@ -60,6 +60,7 @@ class EventController extends Controller
     ->join('personals as per','per.id','=','e.profesional')
     ->whereBetween('e.created_at', [date('Y-m-d 00:00:00', strtotime($f1)), date('Y-m-d 23:59:59', strtotime($f2))])
     ->where('e.sede','=',$request->session()->get('sede'))
+    ->orderBy('EventId','desc')
     ->get();
 
   } else {
@@ -70,6 +71,7 @@ class EventController extends Controller
     ->join('personals as per','per.id','=','e.profesional')
     ->whereDate('e.created_at', '=',Carbon::today()->toDateString())
     ->where('e.sede','=',$request->session()->get('sede'))
+    ->orderBy('EventId','desc')
     ->get();
 
 
@@ -139,7 +141,7 @@ class EventController extends Controller
             ->update([
               'profesional' => $request->especialista,
               'paciente' => $request->paciente,
-              'date' => Carbon::today()->toDateString(),
+              'date' => Carbon::createFromFormat('d/m/Y', $request->date),
               'time' => $request->time,
               'monto' => $request->monto
             ]);
@@ -191,10 +193,11 @@ class EventController extends Controller
   public function ticket_ver($id) 
   {
     $paciente = DB::table('events as e')
-    ->select('e.id as EventId','e.paciente','e.tipo','e.title','e.profesional','e.date','e.monto','e.time','p.dni','p.direccion','p.telefono','p.fechanac','p.gradoinstruccion','p.ocupacion','p.nombres','p.apellidos','p.id as pacienteId','per.name as nombrePro','per.lastname as apellidoPro','per.id as profesionalId','rg.start_time','rg.end_time','rg.id')
+    ->select('e.id as EventId','e.paciente','e.tipo','e.title','e.profesional','e.date','e.monto','e.time','e.created_at','p.dni','p.direccion','p.telefono','p.fechanac','p.gradoinstruccion','p.ocupacion','p.nombres','p.apellidos','p.id as pacienteId','per.name as nombrePro','per.lastname as apellidoPro','per.id as profesionalId','rg.start_time','rg.end_time','rg.id','cr.tipo_ingreso')
     ->join('pacientes as p','p.id','=','e.paciente')
     ->join('personals as per','per.id','=','e.profesional')
     ->join('rangoconsultas as rg','rg.id','=','e.time')
+    ->join('creditos as cr','cr.id_event','e.id')
     ->where('e.id','=',$id)
     ->first();
 
