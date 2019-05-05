236856e80b57579716721b1ffa66239e49d947b3
erik <zerpaerik@gmail.com>
2019-05-05 17:46:41 -0430

    cc

--- app/Http/Controllers/AtencionesController.php
+++ app/Http/Controllers/AtencionesController.php
@@ -266,7 +266,6 @@ public function index(Request $request){
               $s->monto = 99999;
               $s->abono = 0;
               $s->porcentaje =0;
-              $s->sesion =$sesion;
               $s->id_sede =$request->session()->get('sede');
               $s->estatus = 2;
               $s->particular = $request->particular;
@@ -424,7 +423,6 @@ public function index(Request $request){
               $serv->id_paquete =  1;
               $serv->id_servicio =  $servicio['servicio'];
               $serv->es_servicio =  true;
-              $serv->sesion =  $sesion;
               $serv->tipopago = $request->tipopago;
               $serv->porc_pagar = $porcentaje;
               $serv->comollego = $request->comollego;
@@ -689,7 +687,6 @@ public function index(Request $request){
               $s->porc_pagar = 0;
               $s->pendiente = 0;
               $s->monto = 99999;
-              $s->sesion = $sesion;
               $s->abono = 0;
               $s->porcentaje =0;
               $s->id_sede =$request->session()->get('sede');
@@ -868,7 +865,6 @@ $paciente = DB::table('pacientes')
               $serv->id_paquete =  1;
               $serv->id_servicio =  $servicio['servicio'];
               $serv->es_servicio =  true;
-              $serv->sesion =  $sesion;
               $serv->tipopago = $request->tipopago;
               $serv->porc_pagar = $porcentaje;
               $serv->comollego = $request->comollego;
