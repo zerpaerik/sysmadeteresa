63ca17ce30f3eacf587c56777bcc1fab418163d2
erik <zerpaerik@gmail.com>
2019-09-03 19:02:08 -0400

    ticket consulra

--- resources/views/consultas/ticket_consulta.blade.php
+++ resources/views/consultas/ticket_consulta.blade.php
@@ -57,32 +57,50 @@ margin-top: -20px;
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Ticket de Atenciòn</title>
+	<title>Ticket de Consulta</title>
 </head>
 <body>
 
 
-<div class="paciente">
-		<p><strong>Paciente: {{$paciente->apellidos}} {{$paciente->nombres}}</strong></p>
+<div class="" style="font-size: 35px; text-align: center; margin-top: -40px;">
+		<img src="/var/www/html/sysmadeteresa/public/img/image.png"  style="width: 30%;"/>
 	</div>
 
-	<div class="fecha">
-		<p><strong>Fecha de cita: {{ $paciente->date}}</strong></p>
-	</div>
-	<div class="servicios">
-		@if($paciente->tipo == 'CONTROLES')
-		<p><strong>Tipo: CONTROL PRENATAL</strong></p>
-		@else
-		<p><strong>Tipo: CONSULTA</strong></p>
-		@endif
+ <div class="" style="font-size: 30px; text-align: center;margin-bottom:-60px;margin-top: -30px;">
+		<p><strong>MADRE TERESA SAC- {{Session::get('sedeName')}}</strong></p>
+		<p style="margin-top: -20px;"><strong>RUC: 20600971116</strong></p>
+	    <p style="margin-top: -20px;"><strong>DIRECCIÒN: Av Próceres de la independencia 1781 3er piso SJL</strong></p>
+		<p style="margin-top: -20px;"><strong>Teléfono: 01 3764637</strong></p>
+		<p style="margin-top: -20px;"><strong>WhatsApp: 942 066 567</strong></p>
+
 	</div>
 
-	<div class="servicios">
-		<p><strong>Hora de cita: </strong></p>
+	<div class="" style="font-size: 40px; text-align: left;margin-bottom:-50px;">
+		<p><strong>FECHA DE CITA:{{ $paciente->date}}</strong></p>
 	</div>
 
-	<div class="total">
-		<p><strong>Monto: {{ $paciente->monto}}</strong></p>
+	<div class="" style="font-size: 40px; text-align: left;margin-top: -50px;">
+		<p><strong>PACIENTE:{{$paciente->apellidos}} {{$paciente->nombres}}</strong></p>
 	</div>
+   		@if($paciente->tipo == 'CONTROLES')
+
+	<div class="" style="font-size: 40px; text-align: left;margin-top: -50px;">
+		<p><strong>TIPO: CONTROL</strong></p>
+	</div>
+	@else
+
+
+	<div class="" style="font-size: 40px; text-align: left;margin-top: -50px;">
+		<p><strong>TIPO: CONSULTA</strong></p>
+	</div>
+	@endif
+
+	
+
+	<div class="" style="font-size: 40px; text-align: left;margin-top: -50px;;">
+		<p><strong>MONTO: {{ $paciente->monto}}</strong></p>
+	</div>
+
+	
 </body>
 </html>
