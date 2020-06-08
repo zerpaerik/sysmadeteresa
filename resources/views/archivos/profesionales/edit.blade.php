c840af762c303c08294ca9ae4718cf10d0d9501f
zerpaerik <zerpaerik@gmail.com>
2020-06-08 14:46:45 -0400

    Update edit.blade.php

--- resources/views/archivos/profesionales/edit.blade.php
+++ resources/views/archivos/profesionales/edit.blade.php
@@ -33,9 +33,9 @@
 						<div class="col-sm-3">
 							<input type="text" class="form-control" name="apellidos" placeholder="apellidos" data-toggle="tooltip" data-placement="bottom" value="{{$apellidos}}" title="apellidos">
 						</div>
-						<label class="col-sm-1 control-label">Nº Cuenta</label>
+						<label class="col-sm-1 control-label">NºCta</label>
 						<div class="col-sm-3">
-							<input type="text" class="form-control" name="dni" placeholder="dni" data-toggle="tooltip" data-placement="bottom" value="{{$dni}}" title="dni">
+							<input type="text" class="form-control" name="Cuenta" placeholder="dni" data-toggle="tooltip" data-placement="bottom" value="{{$dni}}" title="dni">
 						</div>
 						<label class="col-sm-1 control-label">CMP</label>
 						<div class="col-sm-3">
