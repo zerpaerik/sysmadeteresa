728bb9c5087bf89fc9e67d709eeed7d171431219
zerpaerik <zerpaerik@gmail.com>
2020-06-14 19:24:09 -0400

    Update recibo_profesionales_ver.blade.php

--- resources/views/reportes/recibo_profesionales_ver.blade.php
+++ resources/views/reportes/recibo_profesionales_ver.blade.php
@@ -62,6 +62,8 @@
 
 
  <p style=" margin-top:1px;"><strong>DOCTOR:</strong>{{ $reciboe->name.' '.$reciboe->lastname}}</p>
+  <p style=" margin-top:1px;"><strong>CUENTA:</strong>{{ $reciboe->dni}}</p>
+  <p style=" margin-top:1px;"><strong>TELÈFONO:</strong>{{ $reciboe->phone}}</p>
   <p style="margin-top: -20px;"><strong>CONSULTORIO:</strong>MADRE TERESA</p>
   <p style="margin-top: -20px;"><strong>RECIBO: </strong>{{ $reciboe->recibo}}</p>
 
