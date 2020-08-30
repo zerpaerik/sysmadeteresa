88642773da05166eae82bf4a94a87f1f9389e08f
zerpaerik <zerpaerik@gmail.com>
2020-08-30 19:32:03 -0400

    dddd

--- app/Http/Controllers/Users/UserController.php
+++ app/Http/Controllers/Users/UserController.php
@@ -43,7 +43,7 @@ class UserController extends Controller
 		    $users = DB::table('users as a')
         ->select('a.id','a.estatus','a.name','a.dni','a.id_paciente','a.lastname','a.origen_r','a.validate','a.dni','a.tipo','a.email','a.role_id')
         ->where('a.origen_r','=','APP')
-        ->where('a.id_paciente','=',$request->paciente)
+        ->where('a.dni','=',$request->paciente)
         ->first();  
 
       } else {
