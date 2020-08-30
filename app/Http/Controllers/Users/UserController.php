486f7f94d312809983f2a7d45991d983b32dea17
zerpaerik <zerpaerik@gmail.com>
2020-08-30 19:21:56 -0400

    dddd

--- app/Http/Controllers/Users/UserController.php
+++ app/Http/Controllers/Users/UserController.php
@@ -44,7 +44,7 @@ class UserController extends Controller
         ->select('a.id','a.estatus','a.name','a.dni','a.id_paciente','a.lastname','a.origen_r','a.validate','a.dni','a.tipo','a.email','a.role_id')
         ->where('a.origen_r','=','APP')
         ->where('a.id_paciente','=',$request->paciente)
-        ->get();  
+        ->first();  
 
         print($users);
         die();
