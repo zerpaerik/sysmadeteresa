4f06b3fc60ffc408e80052704a832acee8e9d2c8
zerpaerik <zerpaerik@gmail.com>
2020-08-31 09:30:03 -0400

    dddd

--- app/Http/Controllers/NoticiasController.php
+++ app/Http/Controllers/NoticiasController.php
@@ -70,7 +70,7 @@ class NoticiasController extends Controller
           $noticias->category =$request->category;
           $noticias->date = date('Y-m-d');
           $noticias->usuario = \Auth::user()->id;
-          $noticias->link_img=$nombre_imagen;
+          $noticias->url_img=$nombre_imagen;
           if ($noticias->save()) {
             \Storage::disk('public')->put($nombre_imagen,  \File::get($img));
 
